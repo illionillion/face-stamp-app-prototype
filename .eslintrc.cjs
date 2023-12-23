81950f3ea825f6301176c48a44d7cd92d9a08daa
yuto ichinose <yuto.0526.sano@gmail.com>
2023-12-23 18:09:13 +0900

    comment: コメントアウト

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,7 +5,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
-    'prettier',
+    // 'prettier',
   ],
   ignorePatterns: ['dist', 'docs'],
   parser: '@typescript-eslint/parser',
