08e722ef5cb6edd2f33759cbfeb52e370f38fd60
yuto ichinose <yuto.0526.sano@gmail.com>
2023-12-23 18:04:04 +0900

    feat: prettier追加

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,35 +2,42 @@ module.exports = {
   root: true,
   env: { browser: true, es2021: true, node: true },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
   ],
-  ignorePatterns: ["dist", "docs"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint", "react", "unused-imports", "react-refresh", "import"],
+  ignorePatterns: ['dist', 'docs'],
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+    'react',
+    'unused-imports',
+    'react-refresh',
+    'import',
+  ],
   rules: {
-    "react-refresh/only-export-components": [
-      "warn",
+    'react-refresh/only-export-components': [
+      'warn',
       { allowConstantExport: true },
     ],
-    "react/jsx-uses-react": "off",
-    "react/react-in-jsx-scope": "off",
-    "linebreak-style": ["error", "unix"],
-    quotes: ["error", "single"],
-    semi: ["error", "always"],
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
     // no-mixed-spaces-and-tabsルールをoffに設定する
-    "no-mixed-spaces-and-tabs": "off",
+    'no-mixed-spaces-and-tabs': 'off',
     // // インデントはスペースで統一
-    indent: ["error", 2, { SwitchCase: 1 }],
+    indent: ['error', 2, { SwitchCase: 1 }],
 
-    "import/order": [
-      "error",
+    'import/order': [
+      'error',
       {
-        "alphabetize": {
-          "order": "asc"
-        }
-      }
+        alphabetize: {
+          order: 'asc',
+        },
+      },
     ],
 
     // // タブ文字は使用しない
@@ -41,19 +48,19 @@ module.exports = {
     /**
      * @description importが型のみの場合は、import typeを強制
      */
-    "@typescript-eslint/consistent-type-imports": "error",
+    '@typescript-eslint/consistent-type-imports': 'error',
 
     /**
      * 未使用のimport削除
      */
-    "unused-imports/no-unused-imports": "warn",
+    'unused-imports/no-unused-imports': 'warn',
 
     /**
      * @description propsを自動でソート
      * @see {@link https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/jsx-sort-props.md}
      */
-    "react/jsx-sort-props": [
-      "error",
+    'react/jsx-sort-props': [
+      'error',
       {
         callbacksLast: true,
         shorthandFirst: true,
